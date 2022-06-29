43d8fd5dcb17cabcca692f99c2f90b8a45d50998
unword112 <pog123@hotmail.co.uk>
2022-06-29 16:58:30 +0700

    'final'

---
+++ script.js
@@ -0,0 +1,46 @@
+var xValues = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+var yValues = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48];
+var barColors = 'hsl(10, 79%, 65%)';
+var holdbarColors = 'hsl(186, 34%, 60%)';
+
+new Chart("myChart", {
+    type: "bar",
+    data: {
+      labels: xValues,
+      datasets: [{
+        data: yValues,
+        backgroundColor: barColors,
+        hoverBackgroundColor: holdbarColors,
+        borderRadius: 5,
+        borderSkipped: 5,
+      }],
+    },
+    options: {
+      plugins: {
+          legend: {
+              display: false
+          },
+          tooltip: {
+            enabled: false,
+          },
+        },
+      scales: {
+        x: {
+          grid: {
+            display: false,
+            drawBorder: false,
+          },
+          drawBorder: false   
+        },
+        y: {
+          grid: {
+            display: false,
+            drawBorder: false,
+          },
+          ticks: {
+            display: false,
+          },      
+        }         
+      }
+    }
+});
